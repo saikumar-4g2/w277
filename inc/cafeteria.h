76c4a16ff6a4267201aabe39ee7187c618d79f3a
saikumar-4g2 <saipkumar99@gmail.com>
2020-11-27 12:34:29 +0530

    first

---
+++ inc/cafeteria.h
@@ -0,0 +1,6 @@
+    #ifndef __cafeteria_H__
+	#define __cafeteria_H__
+
+	float cafeteria(int total_rating);
+
+	#endif
---
+++ src/cafeteria.c
@@ -0,0 +1,6 @@
+float cafeteria(int total_rating)
+{
+	float average_rating;
+	average_rating=total_rating/40.0;
+	return average_rating;
+}
